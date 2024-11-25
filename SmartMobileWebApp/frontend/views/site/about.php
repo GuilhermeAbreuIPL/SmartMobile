ba2042860f497ed08026009cd1b2af314660e395
PedroLatadoIPL <2231694@my.ipleiria.pt>
2024-11-25 15:07:37 +0000

    Implementação da Navbar e do Footer + Sobre nós.
    Navbar e footer implementados, + Inicio do desenvolvimento da pagina About us.

--- SmartMobileWebApp/frontend/views/site/about.php
+++ SmartMobileWebApp/frontend/views/site/about.php
@@ -5,12 +5,44 @@
 use yii\helpers\Html;
 
 $this->title = 'About';
-$this->params['breadcrumbs'][] = $this->title;
 ?>
-<div class="site-about">
-    <h1><?= Html::encode($this->title) ?></h1>
+<link rel="stylesheet" href="<?= Yii::getAlias('@web/css/about.css') ?>">
 
-    <p>This is the About page. You may modify the following file to customize its content:</p>
-
-    <code><?= __FILE__ ?></code>
+<div class="container conteudo">
+    <div class="row text-center py-3">
+        <div class="col-md-12">
+            <p class="banner-txt">Quem nós somos?</p>
+            <p class="banner-txt-min">3 alunos de um CeTSP de Programação</p>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col person">
+            <div class="muldura">
+                <img src="#" class="img-responsive" alt="...">
+            </div>
+            <div class="info-perfil">
+                <p>Pedro André Santos Latado</p>
+                <p>Nº 2231619</p>
+            </div>
+        </div>
+        <div class="col person">
+            <div class="muldura">
+                <img src="#" class="img-responsive" alt="...">
+            </div>
+            <div class="info-perfil">
+                <p>Pedro Gaspar</p>
+                <p>Nº 2231991</p>
+            </div>
+        </div>
+        <div class="col person">
+            <div class="muldura">
+                <img src="#" class="img-responsive" alt="...">
+            </div>
+            <div class="info-perfil">
+                <p>Guilherme Nunes Abreu</p>
+                <p>Nº 2232352</p>
+            </div>
+        </div>
+    </div>
 </div>
+
---
+++ SmartMobileWebApp/frontend/web/js/navbar.js
@@ -0,0 +1,31 @@
+// SideBar Menu
+function openNavMenu() {
+    document.getElementById("SidebarMenu").style.width = "400px";
+    document.getElementById("main").style.marginLeft = "400px";
+}
+
+function closeNavMenu() {
+    document.getElementById("SidebarMenu").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+}
+// Sidebar Profile
+function openNavProfile() {
+    document.getElementById("SidebarProfile").style.width = "400px";
+    document.getElementById("main").style.marginLeft = "400px";
+}
+
+function closeNavProfile() {
+    document.getElementById("SidebarProfile").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+}
+
+// Sidebar Profile
+function openNavCart() {
+    document.getElementById("SidebarCart").style.width = "400px";
+    document.getElementById("main").style.marginLeft = "400px";
+}
+
+function closeNavCart() {
+    document.getElementById("SidebarCart").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+}
