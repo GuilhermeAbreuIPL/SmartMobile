8beb84ef95d783ae6036755dc93a83b8fcad9b74
PedroLatadoIPL <2231694@my.ipleiria.pt>
2024-12-04 18:53:01 +0000

    Atualizações Index, footer e sobre nós
    Implementação da barra de rápido acesso a categorias.
    
    Correção do footer.
    
    Implementação da localização no sobre nós

--- SmartMobileWebApp/frontend/views/site/about.php
+++ SmartMobileWebApp/frontend/views/site/about.php
@@ -44,5 +44,10 @@ $this->title = 'About';
             </div>
         </div>
     </div>
-</div>
 
+    <div class="container-map">
+        <div class="row">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3823.304714382037!2d-8.825806751847267!3d39.73443926765741!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd22735a4e067afb%3A0xcfaf619f4450fa76!2sPolit%C3%A9cnico%20de%20Leiria%20%7C%20ESTG%20-%20Escola%20Superior%20de%20Tecnologia%20e%20Gest%C3%A3o_Edif%C3%ADcio%20D!5e1!3m2!1spt-PT!2spt!4v1733326631699!5m2!1spt-PT!2spt" class="google-map" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        </div>
+    </div>
+</div>
--- SmartMobileWebApp/frontend/views/site/index.php
+++ SmartMobileWebApp/frontend/views/site/index.php
@@ -20,6 +20,42 @@ $this->title = 'My Yii Application';
         </div>
     </div>
 
+    <div class="FastAccess-container">
+        <div class="col espaco">
+            <div class="icon-item">
+                <a href="<?= \yii\helpers\Url::to(['site/#']) ?>">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                    <path d="M22 16.92V19a2 2 0 0 1-2.18 2A19.86 19.86 0 0 1 3 5.18 2 2 0 0 1 5 3h2.09a2 2 0 0 1 2 1.72 12.4 12.4 0 0 0 .57 2.53 2 2 0 0 1-.45 2L8 10a16 16 0 0 0 6 6l.75-.75a2 2 0 0 1 2-.45 12.4 12.4 0 0 0 2.52.57 2 2 0 0 1 1.73 2.05z"></path>
+                    </svg>
+                </a>
+                <p>Telemóveis</p>
+            </div>
+        </div>
+        <div class="col espaco">
+            <div class="icon-item">
+                <a href="<?= \yii\helpers\Url::to(['site/#']) ?>">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                        <circle cx="12" cy="12" r="10"></circle>
+                        <path d="M12 6v6l4 2"></path>
+                    </svg>
+                </a>
+                <p>Relógios</p>
+            </div>
+        </div>
+        <div class="col espaco">
+            <div class="icon-item">
+                <a href="<?= \yii\helpers\Url::to(['site/#']) ?>">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                        <path d="M3 7L12 2L21 7L12 12L3 7Z" />
+                        <path d="M3 7V17L12 22L21 17V7" />
+                        <path d="M12 12L12 22" />
+                    </svg>
+                </a>
+                <p>Acessórios</p>
+            </div>
+        </div>
+    </div>
+
     <div class="body-content">
         <div class="row">
             <div class="col-md-auto">
