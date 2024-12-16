40bb5f1a9a38ff5ccd2a4e70841c1a26ab1793f4
PedroLatadoIPL <2231694@my.ipleiria.pt>
2024-12-16 17:16:59 +0000

    Correção das vistas
    Adicionado btn para adicionar ao carrinho
    Correção na barra de navegação rápida

--- SmartMobileWebApp/frontend/views/produto/index.php
+++ SmartMobileWebApp/frontend/views/produto/index.php
@@ -47,7 +47,20 @@
                         <?= \yii\helpers\Html::encode(number_format($precoComDesconto, 2)) ?><small> €</small>
                     </h2>
 
-                    <?= \yii\helpers\Html::a('Mais detalhes', ['view', 'id' => $produto->id], ['class' => 'btnInfo']) ?>
+                    <div class="btnContainer">
+                        <?= \yii\helpers\Html::a('Mais detalhes', ['view', 'id' => $produto->id], ['class' => 'btnInfo']) ?>
+
+                        <?= \yii\helpers\Html::a(
+                            '<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-shopping-cart">
+                                <circle cx="9" cy="21" r="1"></circle>
+                                <circle cx="20" cy="21" r="1"></circle>
+                                <path d="M1 1h4l2.8 12.4a1 1 0 0 0 1 0.6h12a1 1 0 0 0 1-.8L23 6H6"></path>
+                            </svg>',
+                            ['carrinho/add', 'id' => $produto->id],
+                            ['class' => 'btnCart']
+                        ) ?>
+                    </div>
+
                 </div>
             </div>
         </div>
